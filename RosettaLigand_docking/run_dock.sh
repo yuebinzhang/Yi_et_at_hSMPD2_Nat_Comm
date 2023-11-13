3debb258317d6e20a06b0c9904a3036a2a501d37
yuebinzhang <zhangyb@dicp.ac.cn>
2023-11-13 10:17:40 +0800

    Add files via upload

---
+++ RosettaLigand_docking/run_dock.sh
@@ -0,0 +1,2 @@
+export ROSETTA_HOME=/home/zhangyb/Rosetta/rosetta.binary.linux.release-315/
+$ROSETTA_HOME/main/source/bin/rosetta_scripts.static.linuxgccrelease @options.txt  -database $ROSETTA_HOME/main/database/ 1>docking.log 2>error.log 
